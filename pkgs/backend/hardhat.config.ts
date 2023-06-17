b0bbec5f29f911036fd1c5e02cb3ab7be68c6a6d
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-06-17 11:49:18 +0900

    add smartContract source

--- pkgs/backend/hardhat.config.ts
+++ pkgs/backend/hardhat.config.ts
@@ -1,8 +1,49 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from "hardhat/config";
+import 'solidity-coverage';
+
+dotenv.config()
+
+const { 
+  PRIVATE_KEY, 
+  POLYGONSCAN_API_KEY,
+  MUMBAI_API_URL
+} = process.env;
+
+// コンパイル最適化
+const optimizedComilerSettings = {
+  version: '0.8.18',
+  settings: {
+    optimizer: { enabled: true, runs: 1000000 },
+    viaIR: true
+  }
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  etherscan: {
+    apiKey:{
+      polygonMumbai: `${POLYGONSCAN_API_KEY}`
+    }
+  },
+  solidity: {
+    compilers: [{
+      version: '0.8.18',
+      settings: {
+        optimizer: { enabled: true, runs: 1000000 }
+      }
+    }],
+    overrides: {
+      'contracts/samples/SimpleAccount.sol': optimizedComilerSettings
+    }
+  },
+  networks: {
+    mumbai: {
+      url: MUMBAI_API_URL,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  }
 };
 
 export default config;
