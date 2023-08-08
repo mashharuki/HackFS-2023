6c64b6ba47ec33b8b065b9ee9e9779de70172168
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-08-09 00:14:39 +0900

    admin向けの機能を取り除きました

--- pkgs/frontend_user/src/App.tsx
+++ pkgs/frontend_user/src/App.tsx
@@ -2,25 +2,17 @@ import { Polybase } from "@polybase/client";
 import { ethPersonalSign } from '@polybase/eth';
 import { PolybaseProvider } from "@polybase/react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
 import Footer from './Components/common/Footer';
 import Header from './Components/common/Header';
 import Home from './Page/Home';
-import TxnPage from './Page/TxnPage';
 import { ContractProvider } from './context/ContractProvider';
 import { CurrentAccountProvider } from "./context/CurrentAccountProvider";
 import './css/App.css';
-import { DB_NAME_SPACE, GRAPHQL_API_ENDPOINT } from "./utils/Contents";
+import { DB_NAME_SPACE } from "./utils/Contents";
 
 const {
   REACT_APP_CONNECT_ADDRESS_PRIVATE_KEY
 } = process.env;
-
-// create client instance for GraphQL
-const client = new Client({
-  url: GRAPHQL_API_ENDPOINT,
-  exchanges: [cacheExchange, fetchExchange],
-});
 
 // Config of polybase
 const polybase = new Polybase({
@@ -44,7 +36,6 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/transfer' element={<TxnPage/>}/>
       </Routes>
       <Footer/>
     </>
@@ -55,13 +46,11 @@ function Root() {
   return (
     <CurrentAccountProvider>
       <ContractProvider>
-        <Provider value={client}>
-          <PolybaseProvider polybase={polybase}>
-            <BrowserRouter>
-              <App/>
-            </BrowserRouter>
-          </PolybaseProvider>
-        </Provider>
+        <PolybaseProvider polybase={polybase}>
+          <BrowserRouter>
+            <App/>
+          </BrowserRouter>
+        </PolybaseProvider>
       </ContractProvider>
     </CurrentAccountProvider>
   );
--- pkgs/frontend_user/src/Page/Home.tsx
+++ pkgs/frontend_user/src/Page/Home.tsx
@@ -1,36 +1,134 @@
-import { useState } from 'react';
-import { useQuery } from 'urql';
-import FactoryTable from '../Components/FactoryTable';
-import Spinner from '../Components/common/Spinner';
-import query from '../graphql/query';
+import { usePolybase } from "@polybase/react";
+import * as PushAPI from '@pushprotocol/restapi';
+import { useEffect, useState } from "react";
+import ERC20Transfer from "../Components/ERC20Transfer";
+import ERC721Transfer from "../Components/ERC721Transfer";
+import Notifications from '../Components/Notifications';
+import ERC20Table from "../Components/TokenTable/ERC20Table";
+import NftTable from "../Components/TokenTable/NftTable";
+import Transfer from "../Components/Transfer";
+import TxTable from '../Components/TxTable';
+import SliderBar from '../Components/common/Slidebar';
+import Spinner from "../Components/common/Spinner";
+import { loadNotifications } from "../hooks/usePush";
+import { POLYGONSCAN_URL } from "../utils/Contents";
+import { createAlchemy } from "../utils/alchemy";
+import { DB_COLLECTION_NAME, SIMPLE_ACCOUNT_FACTORY_ADDRESS } from './../utils/Contents';
+
+const alchemy = createAlchemy();
 
 /**
- * App Component
+ * Home Component
  * @returns 
  */
-function Home() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  // execute query
-  const [result] = useQuery({ query });
-  const { data, fetching, error } = result;
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tokens, setTokens] = useState<any[]>([]);
+  const [nfts, setNfts] = useState<any[]>([]);
+  const [spams, setSpams] = useState<PushAPI.ParsedResponseType[]>([]);
+  const [txs, setTxs] = useState<any[]>([]);
+  const [notificateFlg, setNotificateFlg] = useState(false);
+  const [txFlg, setTxFlg] = useState(false);
 
-  return (
+  // Polybase config
+  const polybase = usePolybase();
+  // factoryAddressを取得する。
+  const factoryAddress = SIMPLE_ACCOUNT_FACTORY_ADDRESS;
+  // ContractAdderssを取得する。(Web3Authで生成されたアドレスに変えること)
+  const contractAddress = "0xbebfD2FD796F7172152997011D130cb2465eEfCF";
+
+
+  useEffect(() => {
+    const init = async() => {
+      setIsLoading(true);
+
+      // get own's NFT
+      await alchemy.nft.getNftsForOwner(contractAddress).then((res:any) => {
+        setNfts(res.ownedNfts);
+      });
+      // get own's ERC20 token
+      await alchemy.core.getTokenBalances(contractAddress).then((res:any) => {
+        setTokens(res.tokenBalances);
+      });
+
+      // get notifications
+      const notifications = await loadNotifications(false, contractAddress);
+      setSpams(notifications);
+      console.log("spams:", notifications);
+
+      var res = await polybase.collection(`${DB_COLLECTION_NAME}`)
+                          .where("sender", "==", `${contractAddress}`)
+                          .sort("date", "desc")
+                          .get();
+
+      console.log("txs:", res.data);
+      
+      setTxs(res.data);
+      setIsLoading(false);
+    };
+    init();
+  }, []);
+
+  return(
     <div className="App">
       <header className="App-header">
-        {fetching ?
+        {isLoading ? (
           <Spinner/>
-        : 
-          <>
-            {isLoading ? 
-              <Spinner/>
-            :
-              <>{data !== undefined && <FactoryTable data={data} setIsLoading={setIsLoading} />}</>
-            }
-          </>
-        }
+        ) : (
+          <div className='flex flex-row'>
+            <SliderBar 
+              setNotificateFlg={setNotificateFlg} 
+              setTxFlg={setTxFlg} 
+              spams={spams !== undefined ? spams.length : 0}
+              txs={txs !== undefined ? txs.length : 0}
+            />
+            {notificateFlg && (
+              <div>
+                <Notifications spams={spams} />
+              </div>
+            )}
+            {txFlg && (
+              <div>
+                <>{txs !== undefined && <TxTable txs={txs} />}</>
+              </div>
+            )}  
+            {(!notificateFlg && !txFlg ) && (
+              <div>
+                <div className="mb-4">
+                  My Assets
+                </div>
+                <h2 className='text-base'>
+                  factory address: 
+                  <a href={POLYGONSCAN_URL + factoryAddress}>
+                    {factoryAddress}
+                  </a>
+                </h2>
+                <h2 className='text-base mb-8'>
+                  contract address: 
+                  <a href={POLYGONSCAN_URL + contractAddress}>
+                    {contractAddress}
+                  </a>
+                </h2>
+                {/* NFT Balance Table */}
+                <NftTable nfts={nfts} />
+                {/* ERC20 Token Balance Table */}
+                <ERC20Table tokens={tokens} />
+                <div className='flex flex-row gap-16 mt-4 mb-4'>
+                  <Transfer setIsLoading={setIsLoading} factoryAddress={factoryAddress}/>
+                  <ERC20Transfer setIsLoading={setIsLoading} factoryAddress={factoryAddress}/>
+                  <ERC721Transfer setIsLoading={setIsLoading} factoryAddress={factoryAddress}/>
+                </div>
+                <button
+                    className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm"
+                >
+                </button>
+              </div>
+            )}
+          </div>
+        )}
       </header>
     </div>
-  );
+  )
 }
 
 export default Home;
