309a66691f1d689219b4fc783ced70c141d72798
hakua <hakua1452@gmail.com>
2023-08-11 14:39:57 +0900

    frontend_adminからuser向けの機能を取り除く

--- pkgs/frontend_admin/src/App.tsx
+++ pkgs/frontend_admin/src/App.tsx
@@ -1,37 +1,17 @@
-import { Polybase } from "@polybase/client";
-import { ethPersonalSign } from '@polybase/eth';
-import { PolybaseProvider } from "@polybase/react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
 import Footer from './Components/common/Footer';
 import Header from './Components/common/Header';
 import Home from './Page/Home';
-import TxnPage from './Page/TxnPage';
 import { ContractProvider } from './context/ContractProvider';
 import { CurrentAccountProvider } from "./context/CurrentAccountProvider";
 import './css/App.css';
-import { DB_NAME_SPACE, GRAPHQL_API_ENDPOINT } from "./utils/Contents";
-
-const {
-  REACT_APP_CONNECT_ADDRESS_PRIVATE_KEY
-} = process.env;
+import { GRAPHQL_API_ENDPOINT } from "./utils/Contents";
 
 // create client instance for GraphQL
 const client = new Client({
   url: GRAPHQL_API_ENDPOINT,
   exchanges: [cacheExchange, fetchExchange],
-});
-
-// Config of polybase
-const polybase = new Polybase({
-  defaultNamespace: DB_NAME_SPACE,
-});
-
-polybase.signer((data: any) => {
-  return {
-    h: 'eth-personal-sign',
-    sig: ethPersonalSign(`0x${REACT_APP_CONNECT_ADDRESS_PRIVATE_KEY!}`, data)
-  }
 });
 
 /**
@@ -44,7 +24,6 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/transfer' element={<TxnPage/>}/>
       </Routes>
       <Footer/>
     </>
@@ -56,11 +35,9 @@ function Root() {
     <CurrentAccountProvider>
       <ContractProvider>
         <Provider value={client}>
-          <PolybaseProvider polybase={polybase}>
-            <BrowserRouter>
-              <App/>
-            </BrowserRouter>
-          </PolybaseProvider>
+          <BrowserRouter>
+            <App/>
+          </BrowserRouter>
         </Provider>
       </ContractProvider>
     </CurrentAccountProvider>
