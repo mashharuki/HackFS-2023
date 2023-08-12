54eaf7edd6ec3cb5a27017c4aac7f461b345a62f
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-08-12 15:48:49 +0900

    変更

--- pkgs/frontend_user/src/Components/common/Footer/index.tsx
+++ pkgs/frontend_user/src/Components/common/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="p-4 bg-white sm:p-6 dark:bg-gray-800">
 			<div className="sm:flex sm:items-center sm:justify-between">
 				<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-					© 2023 Okodukai Wallet. All Rights Reserved.
+					© 2023 Okodukai Valut. All Rights Reserved.
 				</span>
 				<div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
 					<a href={TWITTER_URL} className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
